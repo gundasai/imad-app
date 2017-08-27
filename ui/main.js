0514f6b28720bb55ca0cdda88561d86642d75d16
gundasai <30929530+gundasai@users.noreply.github.com>
2017-08-27 18:35:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-console.log('Loaded!');
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+
+var button=document.getElementById('counter');
+var span=document.getElementById('count');
+var counting=0;
+button.onclick=function(){
     
+    
+    
+    counting=counting+1;
+    span.ineerHTML=counting.toString();
 };
