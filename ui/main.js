1bb03d6ff7e2ca65aece217c0cbf4b8de7d143bc
gundasai <30929530+gundasai@users.noreply.github.com>
2017-08-27 19:17:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,12 @@
 
 var button=document.getElementById('counter');
+var button1=document.getElementById('submit');
+var text=document.getElementById('name');
 var span=document.getElementById('count');
-
+var name1=document.getElementById('name1');
+var name2=document.getElementById('name2');
+var name3=document.getElementById('name3');
+var submit=0;
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange =function(){
@@ -12,7 +17,20 @@ button.onclick=function(){
             }
             
         }
-    }
+    };
+    button1.onclick=function(){
+        submit=submit+1;
+        var text1=name.innerHTML();
+        if(submit===1)
+        name1.innerHTML=text1;
+        if(submit===2)
+        name2.innerHTML=text1;
+        if(submit===3)
+        name3.innerHTML=text1;
+        
+        
+        
+    };
     
     request.open('GET','http://yeshwanthgunda98.imad.hasura-app.io/counter',true);
     request.send(null);
