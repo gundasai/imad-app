e337ad9f186e2c2863b60a1d10b7c39b97e03e6a
gundasai <30929530+gundasai@users.noreply.github.com>
2017-08-27 17:29:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+var img=document.getElementById('https://pbs.twimg.com/profile_images/901408635426713600/lDWvebXD_bigger.jpg');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+    
+};
