6d7de8a5501b10e44c332a0243495c27c0528c03
gundasai <30929530+gundasai@users.noreply.github.com>
2017-08-27 19:47:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,7 @@
 var button=document.getElementById('counter');
 var button1=document.getElementById('submit');
 var text=document.getElementById('name');
-var span=document.getElementById('count');
-var name1=document.getElementById('name1');
-var name2=document.getElementById('name2');
-var name3=document.getElementById('name3');
-var submit=0;
+
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange =function(){
@@ -25,16 +21,15 @@ button.onclick=function(){
     
    
 };
- button1.onclick=function(){
-        submit=submit+1;
-        var text1=text.value;
-        if(submit===1)
-        name1.innerHTML=text1;
-        if(submit===2)
-        name2.innerHTML=text1;
-        if(submit===3)
-        name3.innerHTML=text1;
-        
-        
-        
+
+ button1.onclick=function() {
+       
+        var names=["name1","name2","name3","name4"];
+        var list=' ' ;
+        for(var i=0;i<names.length;i++){
+            list +='<li>'+names[i]+'</li>';
+            
+        }
+        var ul=document.getElementById('name-list');
+        ul.innerHTML=list;
     };
